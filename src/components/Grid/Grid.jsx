ac32c3cc7553e5cb183029c13e63afc1959699cd
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-03-23 20:27:49 -0500

    adds row position functionality

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -2,31 +2,64 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Plane from '../Plane';
 
-const Grid = ({
-  size = 6,
-  onSquareClick,
-}) => {
-  const Row = ({ y }) => {
-    const r = [];
-    for (let x = 0; x < size; x += 1) {
-      r.push(<Plane onClick={onSquareClick} xPos={x} yPos={y} key={x} />);
-    }
-    return r;
-  };
+const Row = ({ y, length, onClick }) => {
+  const r = [];
+  for (let x = 0; x < length; x += 1) {
+    r.push(<Plane onClick={onClick} xPos={x} yPos={y} key={x} />);
+  }
+  return r;
+};
 
-  const ScaledGrid = () => {
-    const out = [];
-    for (let i = 0; i < size; i += 1) {
-      out.push(<Row y={i} />);
-    }
-    return out;
-  };
+/*
+ size = 3
+ origin = 0,0
+
+ sideLength = (size - 1) / 2
+ OriginRow = -sideLength -> [0,0] => +sideLength
+ */
+const zeroToHalf = (half) => {
+  const out = [];
+  for (let i = 0; i < (half / 2); i += 1) {
+    out.push(i);
+  }
+  return out;
+};
+
+const negToPos = (pos) => {
+  const reversed = [...pos].reverse();
+  const neg = reversed.slice(0, -1).map((i) => -i);
+  return [...neg, ...pos];
+};
+
+
+// const cell = (x, y) => [x, y];
+const placements = [
+  [
+    [-1, -1], [0, -1], [1, -1],
+  ],
+  [
+    [-1, 0], [0, 0], [1, 0],
+  ],
+  [
+    [-1, 1], [0, 1], [1, 1],
+  ],
+];
+
+const makeXs = (size) => negToPos(zeroToHalf((size - 1 / 2)));
+
+const RowData = (xSize, yPosition) => {
+  const xS = makeXs(xSize);
+  return xS.map((x) => [x, yPosition]);
+};
+
+function Grid({ size = 5, onSquareClick }) {
   return (
     <>
-      <ScaledGrid />
+      {placements.map((row) => row.map((cell) => <Plane onClick={onSquareClick} xPos={cell[0]} yPos={cell[1]} key={`plane-row-${row}-cell-${cell}`} />))}
     </>
   );
-};
+}
+
 Grid.propTypes = {
   size: PropTypes.number.isRequired,
   onSquareClick: PropTypes.func.isRequired,
--- src/components/Grid/Grid.spec.jsx
+++ src/components/Grid/Grid.spec.jsx
@@ -3,11 +3,16 @@ import { render } from '@testing-library/react';
 import Grid from '.';
 
 describe('Grid', () => {
-
   it('renders without crashing, matches snapshot', () => {
-    const { container } =  render(<Grid />)
-
+    const { container } = render(<Grid size={6} />);
     expect(container).toMatchSnapshot();
+  });
+  describe.only('size from grid', () => {
+    it('renders without crashing, matches snapshot', () => {
+      const size = 7;
+      const thing = render(<Grid size={size} />);
 
+      expect(thing).toEqual(6);
+    });
   });
 });
